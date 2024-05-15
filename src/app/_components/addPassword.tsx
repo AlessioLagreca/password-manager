ab1908f8090b07d7fb671ecfb4ed0e61e009d288
Alessio Lagreca <82826676+AlessioLagreca@users.noreply.github.com>
2024-05-15 11:59:23 +0200

    added dialog for adding new password

--- src/app/_components/addPassword.tsx
+++ src/app/_components/addPassword.tsx
@@ -5,10 +5,24 @@ import { Toaster, toast } from "sonner";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+const wait = () => new Promise((resolve) => setTimeout(resolve, 1000));
 
 const AggiungiPassword: React.FC = () => {
 	const [servizio, setServizio] = useState("");
 	const [password, setPassword] = useState("");
+	const [open, setOpen] = React.useState(false);
 
 	const handleSubmit = async (event: React.FormEvent) => {
 		event.preventDefault();
@@ -30,31 +44,51 @@ const AggiungiPassword: React.FC = () => {
 	return (
 		<>
 			<Toaster richColors />
-			<form className='mt-4 flex flex-col gap-4 items-center' onSubmit={handleSubmit}>
-				<div className='grid w-full max-w-sm items-center gap-1.5'>
-					<Label htmlFor='servizio'>Servizio</Label>
-					<Input
-						type='text'
-						id='servizio'
-						placeholder='Servizio'
-						value={servizio}
-						onChange={(e) => setServizio(e.target.value)}
-						required
-					/>
-				</div>
-				<div className='grid w-full max-w-sm items-center gap-1.5'>
-					<Label htmlFor='servizio'>Password</Label>
-					<Input
-						type='password'
-						id='password'
-						placeholder='Password'
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-				</div>
-				<Button type='submit'>Aggiungi Elemento</Button>
-			</form>
+			<AlertDialog open={open} onOpenChange={setOpen}>
+				<AlertDialogTrigger>Aggiungi Password</AlertDialogTrigger>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+					</AlertDialogHeader>
+					<form
+						className='mt-4 flex flex-col gap-4 items-center'
+						onSubmit={(e: React.FormEvent) => {
+							wait().then(() => setOpen(false));
+							e.preventDefault();
+							handleSubmit(e);
+						}}
+					>
+						<div className='grid w-full max-w-sm items-center gap-1.5'>
+							<Label htmlFor='servizio'>Servizio</Label>
+							<Input
+								type='text'
+								id='servizio'
+								placeholder='Servizio'
+								value={servizio}
+								onChange={(e) => setServizio(e.target.value)}
+								required
+							/>
+						</div>
+						<div className='grid w-full max-w-sm items-center gap-1.5'>
+							<Label htmlFor='servizio'>Password</Label>
+							<Input
+								type='password'
+								id='password'
+								placeholder='Password'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+						</div>
+						<Button type='submit'>Aggiungi Password</Button>
+
+						<AlertDialogFooter>
+							<AlertDialogCancel>Cancel</AlertDialogCancel>
+							<AlertDialogAction>Continue</AlertDialogAction>
+						</AlertDialogFooter>
+					</form>
+				</AlertDialogContent>
+			</AlertDialog>
 		</>
 	);
 };
