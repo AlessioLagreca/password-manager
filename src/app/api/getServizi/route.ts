1467c306725d29cf8de018a6d0af95067ec313e5
Alessio Lagreca <82826676+AlessioLagreca@users.noreply.github.com>
2024-05-22 21:45:28 +0200

    ciao cache

--- src/app/api/getServizi/route.ts
+++ src/app/api/getServizi/route.ts
@@ -8,6 +8,7 @@ export async function GET(req: NextRequest) {
 			status: 200,
 			headers: {
 				"Content-Type": "application/json",
+				"Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
 			},
 		});
 	} catch (error) {
